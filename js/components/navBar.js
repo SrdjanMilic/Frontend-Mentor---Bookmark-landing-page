5a71bea686ac53d9d81fa5ad09573c34e7fe4c89
Srdjan Milic <srdjanrmilic@gmail.com>
2021-05-05 13:13:50 +0200

    new web component

---
+++ js/components/navBar.js
@@ -0,0 +1,72 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    a,
+    a:-webkit-any-link {
+      text-decoration: none;
+    }
+    
+    :host {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    
+    :host-context(.header) a {
+      color: #2c2748;
+    }
+    
+    :host-context(.footer) {
+      justify-content: left;
+    }
+    
+    :host-context(.footer) a {
+      color: #fff;
+    }
+    
+    :host-context(.footer__container__nav-bar-component) .nav-bar__list {
+      margin-left: 2.7rem;
+    }
+    
+    .nav-bar__logo {
+      width: 148px;
+      height: 25px;
+    }
+    
+    .nav-bar__item {
+      display: inline-block;
+      margin-right: 2.7rem;
+    }
+  </style>
+
+  <img class="nav-bar__logo" />
+  <div class="nav-bar__links">
+    <ul class="nav-bar__list">
+      <li class="nav-bar__item">
+        <a href="#">Components</a>
+      </li>
+      <li class="nav-bar__item">
+        <a href="#">Features</a>
+      </li>
+      <li class="nav-bar__item">
+        <a href="#">Pricing</a>
+      </li>
+      <li class="nav-bar__item">
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+`;
+
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.shadowRoot.querySelector('.nav-bar__logo').src = this.getAttribute('logo');
+  }
+}
+
+window.customElements.define('nav-bar', NavBar);
