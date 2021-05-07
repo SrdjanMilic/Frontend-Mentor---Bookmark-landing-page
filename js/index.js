dca6e3e8424b3d402fd6278bd94079d9f7f94106
Srdjan Milic <srdjanrmilic@gmail.com>
2021-05-07 12:33:39 +0200

    accordion block

--- js/index.js
+++ js/index.js
@@ -2,42 +2,34 @@ const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab => {
-
   tab.addEventListener('click', () => {
     const contentTarget = document.querySelector(tab.dataset.tabTarget);
 
     tabs.forEach(tab => {
       tab.classList.remove('tabs__item--active');
-    })
+    });
 
     tabContents.forEach(content => {
       content.classList.remove('active');
-    })
+    });
 
     contentTarget.classList.add('active');
     tab.classList.add('tabs__item--active');
   });
-})
+});
 
-const accordions = document.querySelectorAll('[data-accordion-target]');
-const accordionContent = document.querySelectorAll('[data-accordion-content]');
+const accordionTitle = document.getElementsByClassName('accordion__title');
 
-accordions.forEach(accordion => {
+for (let i = 0; i < accordionTitle.length; i++) {
+  accordionTitle[i].addEventListener('click', function() {
+    this.classList.toggle('arrow-active');
+    this.classList.toggle('accordion__title-bold');
+    const accordionContent = this.nextElementSibling;
 
-  accordion.addEventListener('click', () => {
-    const contentTarget = document.querySelector(accordion.dataset.accordionTarget);
-    const iconTarget = document.querySelector( '.accordion__arrow-icon');
-
-    accordions.forEach(accordion => {
-      accordion.classList.remove('accordion__item--active');
-    })
-
-    accordionContent.forEach(content => {
-      content.classList.remove('active');
-    })
-
-    contentTarget.classList.add('active');
-    accordion.classList.add('accordion__item--active');
-    iconTarget.classList.add('arrow-active');
+    if (accordionContent.style.maxHeight) {
+      accordionContent.style.maxHeight = null;
+    } else {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+    }
   });
-})
+}
